293bc2d7236bc06b7929449ff176a3a21d31a6d1
MazlumSerbest <mazlumserbest93@gmail.com>
2023-11-27 18:43:03 +0300

    Intefaces changed with types

--- type.d.ts
+++ type.d.ts
@@ -1,6 +1,37 @@
-interface Path {
+type Entity = {
+    id: number;
+    createAt: Date;
+    createdBy: string;
+    updateAt?: Date;
+    updatedBy?: string;
+}
+
+type Path = {
     path: string;
     key: string;
     name: string;
     icon: React.ReactNode;
 }
+
+//#region Table Component Types
+type Column = {
+    name: string;
+    key: string;
+    sortable?: boolean;
+}
+
+type ActiveOption = {
+    name: string;
+    key: string;
+}
+//#endregion
+
+//#region Data Types
+type User = Entity & {
+    email: string;
+    name: string;
+    role: string;
+    active: boolean;
+    avatar: string;
+}
+//#endregion
